de25b13ab3d7a7497d2bb2a128d6dfd7284d1828
Teablack <34287403+Teablack@users.noreply.github.com>
2021-03-11 18:30:26 +0100

    add exercise2

--- WPZI/WPZI_lab2/Exercise2.java
+++ WPZI/WPZI_lab2/Exercise2.java
@@ -1,14 +1,22 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.tika.detect.Detector;
 import org.apache.tika.exception.TikaException;
+import org.apache.tika.io.TikaInputStream;
 import org.apache.tika.langdetect.OptimaizeLangDetector;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.metadata.TikaCoreProperties;
+import org.apache.tika.metadata.TikaMetadataKeys;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.parser.AutoDetectParser;
+import org.apache.tika.parser.ParseContext;
+import org.apache.tika.sax.BodyContentHandler;
 import org.xml.sax.SAXException;
 
 public class Exercise2
@@ -36,11 +44,12 @@ public class Exercise2
 
             File directory = new File("./documents");
             File[] files = directory.listFiles();
+
             for (File file : files)
             {
                 processFile(file);
             }
-        } catch (IOException | SAXException | TikaException e)
+       } catch (IOException | SAXException | TikaException | ParseException e)
         {
             e.printStackTrace();
         }
@@ -50,14 +59,54 @@ public class Exercise2
     private void initLangDetector() throws IOException
     {
         // TODO initialize language detector (langDetector)
+        langDetector = new OptimaizeLangDetector();
+        langDetector.loadModels();
     }
 
-    private void processFile(File file) throws IOException, SAXException, TikaException
-    {
-        // TODO: extract content, metadata and language from given file
-        // call saveResult method to save the data
+    private void processFile(File file) throws IOException, SAXException, TikaException, ParseException {
 
-        saveResult(file.getName(), null, null, null, null, null, null); //TODO: fill with proper values
+        Date creationDate2 = null;
+        Date lastModification2= null;
+        String content="";
+
+        // TODO: extract content, metadata and language from given file
+
+        InputStream stream = new FileInputStream(file);
+        AutoDetectParser adp = new AutoDetectParser();
+        Metadata metadata = new Metadata();
+        BodyContentHandler handler = new BodyContentHandler();
+        adp.parse(stream, handler, metadata, new ParseContext());
+
+        String language= langDetector.detect(handler.toString()).getLanguage();
+
+        metadata.add(Metadata.CREATOR, "Creator");
+        String creatorName= metadata.get(TikaCoreProperties.CREATOR);
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-mm-dd");
+
+        String creationDate1 = metadata.get(TikaCoreProperties.CREATED);
+        //System.out.println(creationDate1);
+        if(creationDate1!=null) {
+            creationDate2= formatter.parse(creationDate1);
+        }
+
+        String lastModification1 = metadata.get(TikaCoreProperties.MODIFIED);
+        //System.out.println(lastModification1);
+        if(lastModification1!=null) {
+            lastModification2 = formatter.parse(lastModification1);
+        }
+        // Define your media type constants here
+
+        Detector detector = adp.getDetector();
+
+        metadata.add(TikaMetadataKeys.RESOURCE_NAME_KEY, file.getName() );
+        TikaInputStream tikaStream = TikaInputStream.get(stream);
+        MediaType mediatype = detector.detect(tikaStream, metadata);
+
+
+
+        // call saveResult method to save the data
+        saveResult(file.getName(), language, creatorName, creationDate2, lastModification2, mediatype.toString(), handler.toString()); //TODO: fill with proper values
     }
 
     private void saveResult(String fileName, String language, String creatorName, Date creationDate,
