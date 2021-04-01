3a0b804ecde7e44b42de3e3ea6f4d6064ea78a90
Teablack <34287403+Teablack@users.noreply.github.com>
2021-04-01 15:53:17 +0200

    Add files via upload

---
+++ WPZI/WPZI_lab7/pr_tr.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+#L1  = [0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+L1  = [0, 1, 1, 0, 1, 0, 0, 0, 0, 0]
+L2  = [1, 0, 0, 1, 0, 0, 0, 0, 0, 0]
+#L3  = [0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+L3  = [0, 1, 0, 0, 0, 0, 1, 0, 0, 0]
+L4  = [0, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+L5  = [0, 0, 0, 0, 0, 1, 1, 0, 0, 0]
+L6  = [0, 0, 0, 0, 0, 0, 1, 1, 0, 0]
+L7  = [0, 0, 0, 0, 1, 1, 1, 1, 1, 1]
+L8  = [0, 0, 0, 0, 0, 0, 1, 0, 1, 0]
+L9  = [0, 0, 0, 0, 0, 0, 1, 0, 0, 1]
+L10 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+L = np.array([L1, L2, L3, L4, L5, L6, L7, L8, L9, L10])
+
+ITERATIONS = 100
+
+def getM(L):
+    M = np.zeros([10, 10], dtype=float)
+    # number of outgoing links
+    c = np.zeros([10], dtype=int)
+    
+    ## TODO 1 compute the stochastic matrix M
+    for i in range(0, 10):
+        c[i] = sum(L[i])
+    
+    for i in range(0, 10):
+        for j in range(0, 10):
+            if L[j][i] == 0: 
+                M[i][j] = 0
+            else:
+                M[i][j] = 1.0/c[j]
+    return M
+    
+print("Matrix L (indices)")
+print(L)    
+
+M = getM(L)
+
+print("Matrix M (stochastic matrix)")
+print(M)
+
+### TODO 2: compute pagerank with damping factor q = 0.15
+### Then, sort and print: (page index (first index = 1 add +1) : pagerank)
+### (use regular array + sort method + lambda function)
+print("PAGERANK")
+
+q = 0.15
+
+pr = np.zeros([10], dtype=float)
+    
+### TODO 3: compute trustrank with damping factor q = 0.15
+### Documents that are good = 1, 2 (indexes = 0, 1)
+### Then, sort and print: (page index (first index = 1, add +1) : trustrank)
+### (use regular array + sort method + lambda function)
+print("TRUSTRANK (DOCUMENTS 1 AND 2 ARE GOOD)")
+
+q = 0.15
+
+d = np.zeros([10], dtype=float)
+
+tr = [v for v in d]
+    
+### TODO 4: Repeat TODO 3 but remove the connections 3->7 and 1->5 (indexes: 2->6, 0->4) 
+### before computing trustrank
